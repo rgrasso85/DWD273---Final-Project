43b2f93d10510acacafcbda8a2a72e3f4b021c8e
rgrass2806 <rgrass2806@WS-AC-CTD-37001>
2017-01-26 09:58:27 -0500

    Created Milestone One Controller/View. Messed with Page CSS

---
+++ StudentPlanner/Controllers/MilestoneOneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentPlanner.Controllers
+{
+    public class MilestoneOneController : Controller
+    {
+        // GET: MilestoneOne
+        public ActionResult MilestoneOne()
+        {
+            return View();
+        }
+    }
+}
