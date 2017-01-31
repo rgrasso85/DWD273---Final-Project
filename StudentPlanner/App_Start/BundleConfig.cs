bfe32df0385d2ab8a043e503ad9c326095f3e111
rgrass2806 <rgrass2806@WS-AC-CTD-37001>
2017-01-31 10:06:04 -0500

    CSS changes

--- StudentPlanner/App_Start/BundleConfig.cs
+++ StudentPlanner/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace StudentPlanner
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
